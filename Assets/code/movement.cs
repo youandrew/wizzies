86dd1343ca04de5c55e48ea28d585a15aa10d165
Andrew the XII <54558003+youandrew@users.noreply.github.com>
2020-01-16 13:12:45 -0600

    cosmic runes

---
+++ Assets/code/movement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [HideInInspector]
+    Vector3 destination;
+    Rigidbody r;
+
+    bool isGrounded = false;
+    public float speed = 1 ;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = gameObject.GetComponent<Rigidbody>();
+        destination = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0,Input.GetAxis("Mouse X"),0,Space.World);
+        if (isGrounded != true)
+        {
+           r.AddForce(-transform.up);
+           
+        }
+         if (Input.GetKey(KeyCode.A))
+        {
+            destination += -transform.right;
+            
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            
+            destination += -transform.forward;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            destination += transform.forward;
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            destination += transform.right;
+            
+        }
+     
+        
+        
+            transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * speed);
+        
+    }
+    void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+
+        }
+
+    }
+}
---
+++ Assets/code/thirdcamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdcamera : MonoBehaviour
+{
+    Camera camObj;
+    string state = "normal";
+    public Transform target;
+
+
+    [HideInInspector]
+    float cameraDelay;
+
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camObj = gameObject.GetComponent<Camera>();
+        offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * cameraDelay);
+
+        transform.LookAt(target.position);
+        //Input.GetAxis("Horizontal");
+    }
+}
